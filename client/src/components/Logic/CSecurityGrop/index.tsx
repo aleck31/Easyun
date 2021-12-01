e836713fa4ec1fba301a2d565b647bd9a4ccde04
Evan <suzukaze.hazuki2020@gmail.com>
2021-12-02 07:35:37 +0900

    chore: remove mui

--- client/src/components/Logic/CSecurityGrop/index.tsx
+++ client/src/components/Logic/CSecurityGrop/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {classnames, TTailwindString} from '@@/tailwindcss-classnames';
 import {Icon} from '@iconify/react';
-import {Checkbox} from '@mui/material';
 
 interface CSecurityGroupProps {
     classes?: TTailwindString;
@@ -9,14 +8,15 @@ interface CSecurityGroupProps {
 
 const CSecurityGroup = (props: CSecurityGroupProps): JSX.Element => {
 	return (
-		<div className={classnames(props.classes, 'border', 'border-yellow-550','rounded', 'p-10', 'pt-5','mt-5')}>
+		<div className={classnames(props.classes, 'border', 'border-yellow-550', 'rounded', 'p-10', 'pt-5', 'mt-5')}>
 			<div className={classnames('pb-10')}>
-				<Icon className={classnames('inline-block','mr-2')} width="25" height="25" color='black' icon="ant-design:lock-outlined" fr={undefined}/>
+				<Icon className={classnames('inline-block', 'mr-2')} width="25" height="25" color='black'
+					icon="ant-design:lock-outlined" fr={undefined}/>
                 easy-sg-manager
 			</div>
-			<div>Enable Ping <Checkbox/></div>
-			<div>Enable SSH<Checkbox/></div>
-			<div className={classnames('pb-10')}>Enable RDP<Checkbox/></div>
+			<div>Enable Ping <input type='checkbox'/></div>
+			<div>Enable SSH<input type='checkbox'/></div>
+			<div className={classnames('pb-10')}>Enable RDP<input type='checkbox'/></div>
 
 			<div>In Bound Port:</div>
 			<div>TCP 660: 0.0.0.0/0</div>
