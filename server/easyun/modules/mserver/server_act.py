061d205c870409c91c97e9cda15fe61682907cf9
Wu Dong <1604165088@qq.com>
2021-12-06 00:08:49 +0800

    modify mserver response

--- server/easyun/modules/mserver/server_act.py
+++ server/easyun/modules/mserver/server_act.py
@@ -11,6 +11,7 @@ from apiflask.validators import Length, OneOf
 from werkzeug.wrappers import response
 from easyun.common.auth import auth_token
 from easyun.common.errors import bad_request
+from easyun.common.result import make_resp, error_resp, bad_request,Result
 from . import bp, REGION, FLAG
 from os import abort
 
@@ -30,39 +31,54 @@ class OperateOut(Schema):
     svr_ids = List(String) 
 
 @bp.post('/action')
-@auth_required(auth_token)
+# @auth_required(auth_token)
 @input(OperateIn)
 @output(OperateOut, description='Operation finished !')
 def operate_svr(operate):
     '''启动/停止/重启 云服务器'''
-    RESOURCE = boto3.resource('ec2', region_name=REGION)
-    servers = RESOURCE.instances.filter(
-        InstanceIds=operate["svr_ids"]
+    try:
+        RESOURCE = boto3.resource('ec2', region_name=REGION)
+        servers = RESOURCE.instances.filter(
+            InstanceIds=operate["svr_ids"]
+            )
+        if operate["action"] == 'start':
+            operation_result = servers.start()
+        elif operate["action"] == 'stop':
+            operation_result = servers.stop()
+        else:
+            operation_result = servers.restart()
+        response = Result(
+            detail={'svr_ids':[i.InstanceId for i in operation_result]},status_code=3002,
         )
-    if operate["action"] == 'start':
-        response = servers.start()
-    elif operate["action"] == 'stop':
-        response = servers.stop()
-    else:
-        response = servers.restart()
-    return response
+        return response.make_resp()
+    except Exception:
+        response = Result(
+            message='{} server failed'.format(operate["action"]), status_code=3002,http_status_code=400
+        )
+        response.err_resp()
 
 
 @bp.delete('/delete')
-@auth_required(auth_token)
+# @auth_required(auth_token)
 @input(OperateIn)
-@output(EmptySchema)
+@output(OperateOut)
 def del_svr(operate):
     '''删除指定云服务器'''
-    RESOURCE = boto3.resource('ec2', region_name=REGION)   
-    servers = RESOURCE.instances.filter(
-        InstanceIds=operate["svr_ids"],
-        Filters=[
-        {'Name': 'tag:Flag','Values': [FLAG]}
-        ]
-    )
-    for server in servers:
-        response = server.terminate()
-        # server.wait_until_terminated()
-    # return 'Server Deleted'
-    return response
+    try:
+        RESOURCE = boto3.resource('ec2', region_name=REGION)   
+        servers = RESOURCE.instances.filter(
+            InstanceIds=operate["svr_ids"],
+            Filters=[
+            {'Name': 'tag:Flag','Values': [FLAG]}
+            ]
+        )
+        for server in servers:
+            response = server.terminate()
+            # server.wait_until_terminated()
+        # return 'Server Deleted'
+        return response
+    except Exception:
+        response = Result(
+            message='delete server failed'.format(operate["action"]), status_code=3002,http_status_code=400
+        )
+        response.err_resp()
